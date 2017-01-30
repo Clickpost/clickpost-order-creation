0060f177d4ce8f0bbc4b2b0d4bbb74743dbf7e3f
Ashok Sahara <ashoksahara@Ashoks-MacBook-Pro.local>
2017-01-30 20:52:38 +0530

    NameSpace Using

---
+++ src/ClickPost/Service/CourierRecommendService.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ashoksahara
+ */
+interface CourierRecommendService {
+    
+    public function getCourierCompany(CourierRecommendData $recommend_data); 
+    //put your code here
+}
+
+?>
